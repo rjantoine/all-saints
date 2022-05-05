b30e8e5fa40eadddd940af37d02f78c63544e533
rantoine <rantoine@mon-avenir.ca>
2022-05-05 10:18:24 -0400

    Work on revalidation api. (7)

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,10 +1,10 @@
 import { isValidRequest } from '@sanity/webhook'
 
 export default async function handler(req, res) {
-    // if (!isValidRequest(req, 'ABC')) {
-    //     const invalidRequest = 'Invalid request'
-    //     return res.status(401).json({ message: invalidRequest })
-    // }
+    if (!isValidRequest(req, 'ABC')) {
+        const invalidRequest = 'Invalid request'
+        return res.status(401).json({ message: invalidRequest })
+    }
 
     const staleRoutes = ['/']
 
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
             staleRoutes.map((route) => res.unstable_revalidate(route))
         )
         const updatedRoutes = `Updated routes: ${staleRoutes.join(', ')}`
-        return res.status(200).json({ success: true, message: JSON.stringify(req), })
+        return res.status(200).json({ success: true, message: updatedRoutes, })
     } catch (err) {
         return res.status(501).json({ error: JSON.stringify(err), message: err.message }).body(JSON.stringify(err))
     }
