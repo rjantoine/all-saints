a29ef24bab23cae749524d64f0b1106d6f3e93fd
rantoine <rantoine@mon-avenir.ca>
2022-05-05 09:27:01 -0400

    Work on revalidation api.

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -20,6 +20,6 @@ export default async function handler(req, res) {
         return res.status(200).json({ message: updatedRoutes })
     } catch (err) {
         log(err.message, true)
-        return res.status(500).json({ error: JSON.stringify(err), message: err.message }).body(JSON.stringify(err))
+        return res.status(501).json({ error: JSON.stringify(err), message: err.message }).body(JSON.stringify(err))
     }
 }
